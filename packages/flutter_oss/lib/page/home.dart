52c3759c7e9e0c21ee72268e0bd1df2a763fd204
yuzheng14 <422450117@qq.com>
2024-08-16 15:22:17 +0800

    perf(flutter): :zap: upload run in pool

--- packages/flutter_oss/lib/page/home.dart
+++ packages/flutter_oss/lib/page/home.dart
@@ -1,10 +1,12 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:uuid/uuid.dart';
+
 import 'package:flutter_oss/model/transfer_task.dart';
 import 'package:flutter_oss/page/storage.dart';
 import 'package:flutter_oss/page/transfer.dart';
-import 'package:http/http.dart' as http;
-import 'package:uuid/uuid.dart';
+import 'package:flutter_oss/util/async_pool.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,45 +18,48 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var selectedIndex = 0;
   List<TransferTask> tasks = [];
+  var pool = AsyncPool(limit: 3);
 
   Future<void> uploadFile() async {
-    var result = await FilePicker.platform.pickFiles();
+    await pool.runInPool(() async {
+      var result = await FilePicker.platform.pickFiles();
 
-    if (result == null) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('no file selected')));
-      return;
-    }
+      if (result == null) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('no file selected')));
+        return;
+      }
 
-    var request = await http.MultipartRequest('PUT',
-        Uri.parse('http://localhost:8000/object/${result.files.single.name}'))
-      ..files.add(
-          await http.MultipartFile.fromPath('file', result.files.single.path!));
+      var request = await http.MultipartRequest('PUT',
+          Uri.parse('http://localhost:8000/object/${result.files.single.name}'))
+        ..files.add(await http.MultipartFile.fromPath(
+            'file', result.files.single.path!));
 
-    var stream = await request.send().catchError((err) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('upload failed: $err'),
-        backgroundColor: Colors.red,
-      ));
-      throw err;
-    });
-
-    var task = TransferTask(
-        id: Uuid().v4(),
-        name: result.files.single.name,
-        type: 'upload',
-        progress: 0);
-
-    setState(() {
-      tasks.add(task);
-    });
-
-    stream.stream.listen((value) {
-      setState(() {
-        task.progress += value.length / result.files.single.size;
+      var stream = await request.send().catchError((err) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('upload failed: $err'),
+          backgroundColor: Colors.red,
+        ));
+        throw err;
       });
-    }).onDone(() {
-      task.progress = 1.0;
+
+      var task = TransferTask(
+          id: Uuid().v4(),
+          name: result.files.single.name,
+          type: 'upload',
+          progress: 0);
+
+      setState(() {
+        tasks.add(task);
+      });
+
+      stream.stream.listen((value) {
+        setState(() {
+          task.progress += value.length / result.files.single.size;
+        });
+      }).onDone(() {
+        task.progress = 1.0;
+      });
     });
   }
 
